67a55255756ac8a322d55270a6ba27386edb13a8
leomitas <leonardo.abreu.dev@gmail.com>
2023-07-08 09:41:48 -0300

    adicionar vercel.json

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,29 @@
-import { NestFactory } from '@nestjs/core'
-import { AppModule } from './app.module'
-import { ValidationPipe } from '@nestjs/common'
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create(AppModule);
 
-  app.enableCors()
+  app.enableCors();
 
   const config = new DocumentBuilder()
-  .setTitle('Contacts')
-  .setDescription('Manage your contacts')
-  .setVersion('1.0')
-  .build()
+    .setTitle('Contacts')
+    .setDescription('Manage your contacts')
+    .setVersion('1.0')
+    .build();
 
-  const document = SwaggerModule.createDocument(app, config)
-  SwaggerModule.setup('api/docs', app, document)
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
 
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true }),
-    new ValidationPipe({ transform: true, transformOptions: {groups: ['transform']} })
-  )
-  await app.listen(process.env.PORT || 3000)
+    new ValidationPipe({
+      transform: true,
+      transformOptions: { groups: ['transform'] },
+    }),
+  );
+  await app.listen(process.env.PORT || 3000);
 }
-bootstrap()
+bootstrap();
